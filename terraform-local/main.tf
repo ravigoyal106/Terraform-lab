3c644479a805ac8766a6f5a3ce8a1f5f3726963f
Ravi Goyal <ravigoyal106@gmail.com>
2026-07-24 18:30:06 +0530

    Learned about the terraform variable

--- terraform-local/main.tf
+++ terraform-local/main.tf
@@ -19,3 +19,17 @@ resource "local_file" "summury" {
   filename = "${path.module}/summury.txt"
   content = "This summury references: ${local_file.Hello.filename}"
 }
+variable "file_content" {
+  description = "Content to write into the file"
+  type = string
+  default = "Hello, This is my terraform first variable test"
+}
+
+resource "local_file" "File1" {
+  filename = "${path.module}/File1.txt"
+  content = var.file_content
+  
+}
+output "File1_file_path" {
+value = local_file.File1.filename  
+}
