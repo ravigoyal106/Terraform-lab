89a298aeccd2470d0d3da2efb2cd93a69b921081
Ravi Goyal <ravigoyal106@gmail.com>
2026-08-01 15:34:38 +0530

    s3 bucket using Terraform

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,21 +1,16 @@
-# Terraform block
-
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-    }
-  }
-}
-
-# Setting aws region 
-provider "aws" {
-  region = "ap-south-1"
-}
-
 # AWS Resource - creating a s3 bucket
 resource "aws_s3_bucket" "test_bucket" {
     bucket = "ravi.awscloudarchitect-2026" 
     # bucket name must be unique
+    tags = {
+      Name = "Learning bucket"
+      Environment = "Learning"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "verexample" {
+  bucket = aws_s3_bucket.test_bucket.id
+  versioning_configuration {
+      status ="Enabled"
+  }
 }
