81e03e460cb2a3dc76a6abcad4779fd2573cd69f
Ravi Goyal <ravigoyal106@gmail.com>
2026-08-02 19:14:58 +0530

    updated ec2/provider

--- terraform-aws/ec2/provider.tf
+++ terraform-aws/ec2/provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hasicorp/aws"
+        source = "hashicorp/aws"
         version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
