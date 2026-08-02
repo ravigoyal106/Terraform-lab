1d3901bca7b6a03946a2e20901f663d7ffa60d65
Ravi Goyal <ravigoyal106@gmail.com>
2026-08-02 15:40:05 +0530

    Commit to /aws

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -13,3 +13,12 @@ resource "aws_s3_bucket_versioning" "verexample" {
       status ="Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.test_bucket.id
+  rule {
+  apply_server_side_encryption_by_default {
+    sse_algorithm = "AES256"
+  }
+  }
+}
